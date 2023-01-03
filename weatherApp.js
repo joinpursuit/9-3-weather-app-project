c113b6920afae6e990b1d160c44af736000c4983
joshuanelsondev <joshuanelsondev@gmail.com>
2023-01-03 16:41:49 -0500

    Update tempAside

--- weatherApp.js
+++ weatherApp.js
@@ -7,8 +7,11 @@ const currentWeatherArticle = document.querySelector('.currentWeather');
 const previousSearches = document.querySelector('.previousSearches');
 const noPreviousSearches = document.querySelector('.noPreviousSearches');
 const searchUlElement = document.querySelector('.searchUl');
-const searchArray = [];
 let tempAside = document.querySelector('.tempAside');
+let todayArticle = document.querySelector('.today');
+let tomorrowArticle = document.querySelector('.tomorrow');
+let dayAfterTomorrowArticle = document.querySelector('.dayAfterTomorrow');
+const searchArray = [];
 let chanceOfSunshine;
 let chanceOfRain;
 let chanceOfSnow;
@@ -37,11 +40,26 @@ function getWeatherInfo(url, location) {
 
 function generateWeatherResults(result, location) {
     currentWeatherArticle.textContent = '';
+    clearTempArticles();
     appendUserInputToMainArticle(location);
     getAreaInfo(result.nearest_area[0], location);
     getRegionInfo(result.nearest_area[0]);
     getCountryInfo(result.nearest_area[0]);
+    getCurrentlyFeelsLikeInfo(result.current_condition[0]);
+    getChanceOfSunshine(result.weather[0].hourly);
+    getChanceOfRain(result.weather[0].hourly);
+    getChanceOfSnow(result.weather[0].hourly);
+    appendPreviousSearch(result, location);
+    generateTodayCard(result.weather[0]);
+    generateTomorrowCard(result.weather[1]);
+    generateDayAfterTomorrowCard(result.weather[2]);
     form.reset();
+}
+
+function appendUserInputToMainArticle(userInput) {
+    const searchInput = document.createElement('h2');
+    currentWeatherArticle.append(searchInput);
+    searchInput.textContent = userInput;
 }
 
 function getAreaInfo(result, location) {
@@ -75,10 +93,177 @@ function getCountryInfo(result) {
     currentWeatherArticle.append(country);
 }
 
-function appendUserInputToMainArticle(userInput) {
-    const searchInput = document.createElement('h2');
-    currentWeatherArticle.append(searchInput);
-    searchInput.textContent = userInput;
+function getCurrentlyFeelsLikeInfo(result) {
+    const currentlyFeelsLike = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerHTML = "Country:  ";
+    currentlyFeelsLike.append(strong, 'Feels Like ', result.FeelsLikeF, '°F');
+    currentWeatherArticle.append(currentlyFeelsLike);
+}
+
+function getChanceOfSunshine(chanceArray) {
+    const avgChanceOfSun = chanceArray.reduce((totalChance, chanceObj) => {
+        return totalChance + Number(chanceObj.chanceofsunshine);
+    }, 0) / chanceArray.length;
+
+    if (avgChanceOfSun > 50) {
+        const sunnyIcon = document.createElement('img');
+        sunnyIcon.setAttribute('src', './assets/icons8-summer.gif');
+        sunnyIcon.setAttribute('alt', 'sun');
+        currentWeatherArticle.prepend(sunnyIcon);
+    }
+
+    chanceOfSunshine = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerHTML = 'Chance of Sunshine: ';
+    chanceOfSunshine.append(strong, avgChanceOfSun.toFixed(), '%');
+    currentWeatherArticle.append(chanceOfSunshine);
+}
+
+function getChanceOfRain(chanceArray) {
+    const avgChanceOfRain = chanceArray.reduce((totalChance, chanceObj) => {
+        return totalChance + Number(chanceObj.chanceofrain);
+    }, 0) / chanceArray.length;
+
+    if (avgChanceOfRain > 50) {
+        const rainIcon = document.createElement('img');
+        rainIcon.setAttribute('src', './assets/icons8-rain-cloud.gif');
+        rainIcon.setAttribute('alt', 'rain');
+        currentWeatherArticle.prepend(rainIcon);
+    }
+
+    chanceOfRain = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerHTML = 'Chance of Rain: ';
+    chanceOfRain.append(strong, avgChanceOfRain.toFixed(), '%');
+    currentWeatherArticle.append(chanceOfRain);
+}
+
+function getChanceOfSnow(chanceArray) {
+    const avgChanceOfSnow = chanceArray.reduce((totalChance, chanceObj) => {
+        return totalChance + Number(chanceObj.chanceofsnow);
+    }, 0) / chanceArray.length;
+
+    if (avgChanceOfSnow > 50) {
+        const snowIcon = document.createElement('img');
+        snowIcon.setAttribute('src', './assets/icons8-light-snow.gif');
+        snowIcon.setAttribute('alt', 'snow');
+        currentWeatherArticle.prepend(snowIcon);
+    }
+
+    chanceOfSnow = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerHTML = 'Chance of Snow: ';
+    chanceOfSnow.append(strong, avgChanceOfSnow.toFixed(), '%');
+    currentWeatherArticle.append(chanceOfSnow);
+}
+
+function appendPreviousSearch(result, location) {
+    const searchLiElement = document.createElement('li');
+
+    if (!searchArray.includes(location)) {
+        const anchorTagForSearchLink = document.createElement('a');
+        anchorTagForSearchLink.setAttribute('href', '#');
+        anchorTagForSearchLink.setAttribute('name', currentApi);
+        anchorTagForSearchLink.classList.add('previousSearchLink');
+        searchLiElement.append(anchorTagForSearchLink);
+        anchorTagForSearchLink.textContent = location;
+        anchorTagForSearchLink.after(` - ${result.current_condition[0].FeelsLikeF}°F`);
+        noPreviousSearches.textContent = '';
+        searchUlElement.append(searchLiElement);
+        searchArray.push(location);
+        anchorTagForSearchLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            const locationH2Element = document.querySelector('h2');
+            if (locationH2Element.textContent !== location) {
+                getWeatherInfo(`${event.target.name}`, location);
+            }
+            searchUlElement.append(searchLiElement);
+        });
+    }
+
+    if (searchArray.length > 10) {
+        removePreviousSearch();
+    }
+}
+
+function generateTodayCard(result) {
+    // const todayDivElement = document.createElement('div');
+    todayArticle.classList.add('tempCard');
+    const todayHeader = document.createElement('h3');
+    const averageTemp = document.createElement('p');
+    const maxTemp = document.createElement('p');
+    const minTemp = document.createElement('p');
+    // const strongElement = document.createElement('strong');
+    // todayDivElement.classList.add('tempDivs');
+    todayHeader.textContent = 'Today';
+    averageTemp.innerHTML = `<strong>Average Temperature:<strong>`;
+    maxTemp.innerHTML = `<strong>Max Temperature:<strong>`;
+    minTemp.innerHTML = `<strong>Min Temperature:<strong>`;
+    averageTemp.append(`${result.avgtempF} °F`);
+    maxTemp.append(`${result.maxtempF} °F`);
+    minTemp.append(`${result.mintempF} °F`);
+
+    todayArticle.append(todayHeader, averageTemp, maxTemp, minTemp);
+    tempAside.append(todayArticle);
+}
+
+function generateTomorrowCard(result) {
+    // const tomorrowDivElement = document.createElement('div');
+    tomorrowArticle.classList.add('tempCard');
+    const tomorrowHeader = document.createElement('h3');
+    const averageTemp = document.createElement('p');
+    const maxTemp = document.createElement('p');
+    const minTemp = document.createElement('p');
+    // const strongElement = document.createElement('strong');
+    // tomorrowDivElement.classList.add('tempDivs');
+    tomorrowHeader.textContent = 'Tomorrow';
+    averageTemp.innerHTML = `<strong>Average Temperature:<strong>`;
+    maxTemp.innerHTML = `<strong>Max Temperature:<strong>`;
+    minTemp.innerHTML = `<strong>Min Temperature:<strong>`;
+    averageTemp.append(`${result.avgtempF} °F`);
+    maxTemp.append(`${result.maxtempF} °F`);
+    minTemp.append(`${result.mintempF} °F`);
+
+    tomorrowArticle.append(tomorrowHeader, averageTemp, maxTemp, minTemp);
+    tempAside.append(tomorrowArticle);
+}
+
+function generateDayAfterTomorrowCard(result) {
+    // const dayAfterTomorrowDivEle = document.createElement('div');
+    dayAfterTomorrowArticle.classList.add('tempCard');
+    const dayAfterTomorrowHeader = document.createElement('h3');
+    const averageTemp = document.createElement('p');
+    const maxTemp = document.createElement('p');
+    const minTemp = document.createElement('p');
+    // const strongElement = document.createElement('strong');
+    // dayAfterTomorrowDivEle.classList.add('tempDivs');
+    dayAfterTomorrowHeader.textContent = 'Day After Tomorrow';
+    averageTemp.innerHTML = `<strong>Average Temperature:<strong>`;
+    maxTemp.innerHTML = `<strong>Max Temperature:<strong>`;
+    minTemp.innerHTML = `<strong>Min Temperature:<strong>`;
+    averageTemp.append(`${result.avgtempF} °F`);
+    maxTemp.append(`${result.maxtempF} °F`);
+    minTemp.append(`${result.mintempF} °F`);
+
+    dayAfterTomorrowArticle.append(dayAfterTomorrowHeader, averageTemp, maxTemp, minTemp);
+    tempAside.append(dayAfterTomorrowArticle);
+}
+
+function clearTempArticles() {
+    [todayArticle, tomorrowArticle, dayAfterTomorrowArticle].forEach(article => article.remove());
+    todayArticle = document.createElement('article');
+    tomorrowArticle = document.createElement('article');
+    dayAfterTomorrowArticle = document.createElement('article');
+    todayArticle.classList.add('today');
+    tomorrowArticle.classList.add('tomorrow');
+    dayAfterTomorrowArticle.classList.add('dayAfterTomorrow');
+    tempAside.append(todayArticle, tomorrowArticle, dayAfterTomorrowArticle);
+}
+
+function removePreviousSearch() {
+    searchUlElement.firstChild.remove();
+    searchArray.shift();
 }
 
 function capitalizeFirstLetter(string) {
