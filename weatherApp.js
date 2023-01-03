82e4f99f8fd54131adb31975b5537798fbd6dc00
joshuanelsondev <joshuanelsondev@gmail.com>
2023-01-03 12:58:10 -0500

    Create functions that will update the search info

--- weatherApp.js
+++ weatherApp.js
@@ -8,7 +8,7 @@ const previousSearches = document.querySelector('.previousSearches');
 const noPreviousSearches = document.querySelector('.noPreviousSearches');
 const searchUlElement = document.querySelector('.searchUl');
 const searchArray = [];
-let tempSection = document.querySelector('.tempSection');
+let tempAside = document.querySelector('.tempAside');
 let chanceOfSunshine;
 let chanceOfRain;
 let chanceOfSnow;
@@ -17,5 +17,74 @@ let currentApi;
 form.addEventListener('submit', generateUrl);
 
 function generateUrl(event) {
-    
+    event.preventDefault();
+    const location = capitalizeFirstLetter(`${searchField.value}`);
+    currentApi = `${BASE_URL}${location}?format=j1`;
+    getWeatherInfo(currentApi, location);
+}
+
+function getWeatherInfo(url, location) {
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => {
+            generateWeatherResults(result, location);
+        })
+        .catch((error) => {
+            createErrorMessage(error);
+        });
+}
+
+function generateWeatherResults(result, location) {
+    currentWeatherArticle.textContent = '';
+    appendUserInputToMainArticle(location);
+    getAreaInfo(result.nearest_area[0], location);
+    getRegionInfo(result.nearest_area[0]);
+    getCountryInfo(result.nearest_area[0]);
+    form.reset();
+}
+
+function getAreaInfo(result, location) {
+    const areaElement = document.createElement('p');
+    const strong = document.createElement('strong');
+    const areaName = result.areaName[0].value;
+
+    if (location === areaName) {
+        strong.innerHTML = "Area:  ";
+    } else {
+        strong.innerHTML = "Nearest Area: ";
+    }
+
+    areaElement.append(strong, areaName);
+    currentWeatherArticle.append(areaElement);
+}
+
+function getRegionInfo(result) {
+    const region = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerHTML = "Region:  ";
+    region.append(strong, result.region[0].value);
+    currentWeatherArticle.append(region);
+}
+
+function getCountryInfo(result) {
+    const country = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerHTML = "Country:  ";
+    country.append(strong, result.country[0].value);
+    currentWeatherArticle.append(country);
+}
+
+function appendUserInputToMainArticle(userInput) {
+    const searchInput = document.createElement('h2');
+    currentWeatherArticle.append(searchInput);
+    searchInput.textContent = userInput;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ');
+}
+
+function createErrorMessage(error) {
+
 }
