e6436f67a69596f8064a45bcbba0b6455a0e3bd4
HaroldF415 <harold.f415@gmail.com>
2023-01-03 13:55:16 -0500

    functionality

---
+++ main.js
@@ -0,0 +1,5 @@
+const weatherF = document.querySelector("#weather_form");
+weatherF.addEventListener("submit", handleWeatherSubmit);
+
+const tempConvF = document.querySelector("#temp_form");
+tempConvF.addEventListener("submit", handleTempSubmit);
