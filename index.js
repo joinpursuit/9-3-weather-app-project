af90f9bb6eca34e35e94f526c9f3a68d5851f153
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-01-04 13:40:52 -0500

    Add three day temperature forecast.

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 let textfield = document.querySelector("#cityInput");
-let infoOutput = document.querySelector("main");
+let currentOutput = document.querySelector("#current");
+let todayOutput = document.querySelector("#today");
+let tomorrowOutput = document.querySelector("#tomorrow");
+let dayAfterOutput = document.querySelector("#afterThat")
 document.addEventListener("submit",(e)=>{
     e.preventDefault();
     let requestString = "https://wttr.in/" + textfield.value + "?format=j1";
     fetch(requestString).then((response) => response.json()).then((data) => {
         console.log(data)
-        infoOutput.innerHTML = 
+        currentOutput.innerHTML = 
         `
-        <p><strong>Are: </strong>${data.nearest_area[0].areaName[0].value}<p>
+        <h2>${data.nearest_area[0].areaName[0].value}</h2>
+        <p><strong>Area: </strong>${data.nearest_area[0].areaName[0].value}<p>
         <p><strong>Region: </strong>${data.nearest_area[0].region[0].value}<p>
         <p><strong>Country: </strong>${data.nearest_area[0].country[0].value}<p>
         <p><strong>Currently: </strong> Feels Like ${data.current_condition[0].FeelsLikeF}°<p>
+        `
+
+        todayOutput.innerHTML = 
+        `
+        <h3>Today</h3>
+        <p><strong>Average Temperature: </strong>${data.weather[0].avgtempF}</p>
+        <p><strong>Max Temperature: </strong>${data.weather[0].maxtempF}</p>
+        <p><strong>Min Temperature: </strong>${data.weather[0].mintempF}</p>
+        `
+        
+        tomorrowOutput.innerHTML = 
+        `
+        <h3>Tomorrow</h3>
+        <p><strong>Average Temperature: </strong>${data.weather[1].avgtempF}</p>
+        <p><strong>Max Temperature: </strong>${data.weather[1].maxtempF}</p>
+        <p><strong>Min Temperature: </strong>${data.weather[1].mintempF}</p>
+        `
+
+        dayAfterOutput.innerHTML = 
+        `
+        <h3>Day After Tomorrow</h3>
+        <p><strong>Average Temperature: </strong>${data.weather[2].avgtempF}</p>
+        <p><strong>Max Temperature: </strong>${data.weather[2].maxtempF}</p>
+        <p><strong>Min Temperature: </strong>${data.weather[2].mintempF}</p>
         `
     })
 });
