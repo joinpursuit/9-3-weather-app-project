1089daf48454b9521d0563e660057c3af23ded5e
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-01-07 15:04:43 -0500

    Fetched API finally got it working!!

--- weather.js
+++ weather.js
@@ -1,11 +1,23 @@
-const BASE_URL = "https://wttr.in/"
+const BASE_URL = "https://wttr.in"
 const form = document.querySelector("form");
 const sidebar = document.querySelector(".side-bar")
+const userInput = document.querySelector("input")
 form.addEventListener("submit", (event)=> {
     event.preventDefault();
-    const p = document.createElement("p");
-    const {location} = event.target
-    sidebar.append("p")
-
+   
+    const {location} = userInput.value
+    
+    getWeatherByLocation(location);
+    form.reset();
 })
-function getWeatherByLocation()
+
+function getWeatherByLocation(location) {
+    fetch(`${BASE_URL}/${location}?format=j1`)
+    .then((response) =>
+       response.json())
+    .then((resultInJS) => {
+        console.log(resultInJS)
+    })
+    .catch((error) => 
+        console.log(error))
+}   
