4578ec41cfeadf43043d45a5067d231dd455ea22
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-01-07 00:22:50 -0500

    add some code to main.js as well as add data to styling.css

--- main.js
+++ main.js
@@ -15,6 +15,21 @@ function weatherDisplay(userInput) {
     fetch(`https://wttr.in/${userInput}?format=j1`)
     .then((res) => res.json())
     .then((file) => {
+
+        const currentWeather =document.querySelector("article.current-weather");
+        let city = document.createElement("h2");
+        city.textContent = userInput;
+        city.textContent = userInput;
+        currentWeather.append(city);
+
+        let area = document.createElement("p");
+        area.innerHTML = file.nearest_area[0].areaName[0].value.toLowerCase() === userInput.toLowerCase()
+        ? `<strong>area:</strong>${file.nearest_area[0].areaName[0].value}`
+        : `<strong>Nearest Area:</strong>${file.nearest_area[0].areaName[0].value}`;
+
+        currentWeather.append(area);
+
         
+
     })
 }
