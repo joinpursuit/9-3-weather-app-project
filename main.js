36904d5bddc26e1ebd6f1a71ae58d330b568083c
SangUn Park <Sangunpark1@gmail.com>
2023-01-10 12:06:27 -0500

    Sunshine, rain or snow gif code

--- main.js
+++ main.js
@@ -52,4 +52,35 @@ form.addEventListener("submit", (event) => {
             const { chanceofrain, chanceofsunshine, chanceofsnow } =
             weatherInfo.weather[0].hourly[0];
     
-          
+            const pRain = document.createElement("p");
+            const pSunshine = document.createElement("p");
+            const pSnow = document.createElement("p");
+      
+            pSunshine.innerHTML = "<strong>Chance of Sunshine: </strong>" + chanceofsunshine + "%";
+            pRain.innerHTML = "<strong>Chance of Rain: </strong>" + chanceofrain + "%";
+            pSnow.innerHTML = "<strong>Chance of Snow: </strong>" + chanceofsnow + "%";
+            mainArticle.append(pSunshine, pRain, pSnow);
+      
+            // Prepend an icon based on weather logic
+            const img = document.createElement("img");
+            if (chanceofsunshine > 50) {
+              // display summer icon
+              // img = '<img src="./assets/icons8-summer.gif" alt="sun" />';
+              img.setAttribute("src", "./assets/icons8-summer.gif");
+              img.setAttribute("alt", "sun");
+            } else if (chanceofrain > 50) {
+              // img = '<img src="./assets/icons8-torrential-rain.gif" alt="rain" />';
+              img.setAttribute("src", "./assets/icons8-torrential-rain.gif");
+              img.setAttribute("alt", "rain");
+            } else if (chanceofsnow > 50) {
+              // img = '<img src="./assets/icons8-light-snow.gif" alt="snow" />';
+              img.setAttribute("src", "./assets/icons8-light-snow.gif");
+              img.setAttribute("alt", "snow");
+            }
+            mainArticle.append(img);
+      
+            const mainAside = document.querySelector("main aside")
+      
+            mainAside.innerHTML = '';
+      
+             
