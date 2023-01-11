eafcc963bb57b5690dd782aab1b27ebfc6be97fc
vg0at <cameronmedina0130@gmail.com>
2023-01-10 23:53:49 -0500

    Previous Searches list is functional now,  but still produces duplicate <li> elements. Added hidden attribute to conversion widget temporarily until JS Functionality is added. Elements with temperature values also now contain a ° (degree) symbol.

--- function.js
+++ function.js
@@ -12,7 +12,7 @@ const current = document.querySelector(".current");
 
 
 const divForecast = document.querySelector("main div.fore");
-console.log(divForecast)
+// console.log(divForecast)
 
 
 // Function that displays the template for our 3-day Forecast
@@ -26,8 +26,28 @@ const inputHeading = document.createElement("h2");
 // current.prepend(inputHeading);
 // inputHeading.innerHTML = "";
 const currentSection = document.querySelector(".view-weather section");
-console.log(currentSection)
+// console.log(currentSection)
 currentSection.append(inputHeading, areaP, regionP, countryP, currentlyFP);
+
+
+// Selector for <aside> Previous Searches 
+// const ul = document.querySelector("ul")
+// console.log(list)
+
+
+
+// function createLink(search) {
+//     //Take the Value of City name, connected to a link containing the BASE_URL used in fetch.
+//   const a = document.createElement("a");
+//   const p = document.createElement("p");
+  
+//   const li = document.querySelector("li")
+//   li.append(p);
+  
+  
+// }
+
+
 
 /* function unhideCurrentClass(pclass)
  - (pclass) {user input => json}
@@ -103,10 +123,86 @@ const dayAfterMinTemp = document.createElement("p");
 articleDayAfter.append(dayAfterAvgTemp, dayAfterMaxTemp, dayAfterMinTemp)
 
 
+function removeP(empty){ 
+    if (empty) {
+        empty.remove();
+    }
+}
+
+
+// function getWeather(res){
+//     const input = document.querySelector('input').value
+//     const prettyInput = `${editValue(input)}`
+//     inputHeading.innerHTML = prettyInput; 
+
+//     const nearestArea = res.nearest_area[0].areaName[0].value;
+//     areaP.innerHTML = `<strong>Area: </strong>${nearestArea}`;
+
+//     const region = res.nearest_area[0].region[0].value;
+//     regionP.innerHTML = `<strong>Region: </strong>${region}`
+
+//     const country = res.nearest_area[0].country[0].value;
+//     // console.log(country + ": country");
+//     countryP.innerHTML = `<strong>Country:</strong> ${country}`
+    
+//     const currentlyF = res.current_condition[0].FeelsLikeF;
+//     // console.log(currentlyF + ": currentlyF")
+//     currentlyFP.innerHTML = `<strong>Currently: </strong>${currentlyF}`
+
+
+//         // Adding data to 3-Day Forecast Elements! 
+
+//         // ** TODAY **
+
+//         // Average
+//         const todayAvgTempJson = json.weather[0].avgtempF;
+//         todayAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${todayAvgTempJson}`;
+
+//         // Max
+//         const todayMaxTempJson = json.weather[0].maxtempF;
+//         todayMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${todayMaxTempJson}`;
+
+//         // Min 
+//         const todayMinTempJson = json.weather[0].mintempF;
+//         todayMinTemp.innerHTML = `<strong>Min Temperature: </strong> ${todayMinTempJson}`
+    
+//         // ** TOMORROW ** 
+
+//         // Average
+//         const tmmAvgTempJson = json.weather[1].mintempF;
+//         tmmAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${tmmAvgTempJson}`
+//         // Max 
+//         const tmmMaxTempJson = json.weather[1].maxtempF;
+//         tmmMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${tmmMaxTempJson}`
+
+//         // Min
+//         const tmmMinTempJson = json.weather[1].mintempF;
+//         tmmMinTemp.innerHTML = `<strong>Min Temperature: </strong>${tmmMinTempJson}`
+
+//         // ** DAY AFTER TOMORROW **
+
+//         // Average
+//         const dayAfterAvgTempJson = json.weather[2].avgtempF;
+//         dayAfterAvgTemp.innerHTML = innerHTML = `<strong>Average Temperature: </strong>${dayAfterAvgTempJson}`
+
+//         // Max 
+//         const dayAfterMaxTempJson = json.weather[2].maxtempF;
+//         dayAfterMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${dayAfterMaxTempJson}`
+
+//         //Min
+
+//         const dayAfterMinTempJson = json.weather[2].mintempF;
+//         dayAfterMinTemp.innerHTML = `<strong>Min Temperature: </strong>${dayAfterMinTempJson}`
+// }
 
 
 
 // function updateForecast(data) {
-    //  console.log(response.nearest_area[0].areaName[0].value);
-    // }
+    // let chars = ['A', 'B', 'A', 'C', 'B'];
+    // let uniqueChars = [...new Set(chars)];
+    
+    // console.log(uniqueChars); 
+       // }
  // updateForecast(json);
+
+
--- script.js
+++ script.js
@@ -40,9 +40,10 @@ const countryP = document.createElement("p");
 const currentlyFP = document.createElement("p");
 
 // currentSection.append(inputHeading, areaP, regionP, countryP, currentlyFP);
- 
 
 
+
+// const listArr = [];
 search.addEventListener("submit", (event) => {
     event.preventDefault();
     
@@ -67,16 +68,20 @@ search.addEventListener("submit", (event) => {
     fetch(BASE_URL)
     .then((response) => response.json())
     .then((json) => {
-        
+        console.log(BASE_URL)
         // Removes "Choose a location..." <p> that appears on landing page.
-        const empty = document.querySelector('p.empty');
-        if (empty) {
-            empty.remove();
-            // unhideCurrentClass(current);
+        const emptyC = document.querySelector('.current p.empty');
+       
+        const emptyUl = document.querySelector("ul p.empty");
+        removeP(emptyC);
+        removeP(emptyUl);
+
+
+        removeHidden(current);
+        removeHidden(divForecast);
+        
             // if (divForecast.hasAttribute("hidden"))
                 // unhideForecast(divForecast);
-            removeHidden(current);
-            removeHidden(divForecast);
 
 
             console.log(prettyInput + ": prettyInput")
@@ -107,10 +112,10 @@ search.addEventListener("submit", (event) => {
             
             const currentlyF = json.current_condition[0].FeelsLikeF;
             // console.log(currentlyF + ": currentlyF")
-            currentlyFP.innerHTML = `<strong>Currently: </strong>${currentlyF}`
+            currentlyFP.innerHTML = `<strong>Currently: </strong>Feels like ${currentlyF}°F`
             
             
-            console.log(areaP);
+            // console.log(areaP);
             
         // Adding data to 3-Day Forecast Elements! 
 
@@ -118,51 +123,174 @@ search.addEventListener("submit", (event) => {
 
         // Average
         const todayAvgTempJson = json.weather[0].avgtempF;
-        todayAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${todayAvgTempJson}`;
+        todayAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${todayAvgTempJson}°F`;
 
         // Max
         const todayMaxTempJson = json.weather[0].maxtempF;
-        todayMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${todayMaxTempJson}`;
+        todayMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${todayMaxTempJson}°F`;
 
         // Min 
         const todayMinTempJson = json.weather[0].mintempF;
-        todayMinTemp.innerHTML = `<strong>Min Temperature: </strong> ${todayMinTempJson}`
+        todayMinTemp.innerHTML = `<strong>Min Temperature: </strong> ${todayMinTempJson}°F`
     
         // ** TOMORROW ** 
 
         // Average
         const tmmAvgTempJson = json.weather[1].mintempF;
-        tmmAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${tmmAvgTempJson}`
+        tmmAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${tmmAvgTempJson}°F`
         // Max 
         const tmmMaxTempJson = json.weather[1].maxtempF;
-        tmmMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${tmmMaxTempJson}`
+        tmmMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${tmmMaxTempJson}°F`
 
         // Min
         const tmmMinTempJson = json.weather[1].mintempF;
-        tmmMinTemp.innerHTML = `<strong>Min Temperature: </strong>${tmmMinTempJson}`
+        tmmMinTemp.innerHTML = `<strong>Min Temperature: </strong>${tmmMinTempJson}°F`
 
         // ** DAY AFTER TOMORROW **
 
         // Average
         const dayAfterAvgTempJson = json.weather[2].avgtempF;
-        dayAfterAvgTemp.innerHTML = innerHTML = `<strong>Average Temperature: </strong>${dayAfterAvgTempJson}`
+        dayAfterAvgTemp.innerHTML = innerHTML = `<strong>Average Temperature: </strong>${dayAfterAvgTempJson}°F`
 
         // Max 
         const dayAfterMaxTempJson = json.weather[2].maxtempF;
-        dayAfterMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${dayAfterMaxTempJson}`
+        dayAfterMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${dayAfterMaxTempJson}°F`
 
         //Min
 
         const dayAfterMinTempJson = json.weather[2].mintempF;
-        dayAfterMinTemp.innerHTML = `<strong>Min Temperature: </strong>${dayAfterMinTempJson}`
+        dayAfterMinTemp.innerHTML = `<strong>Min Temperature: </strong>${dayAfterMinTempJson}°F`
         
             
             
+        // add previously searched data to list
+        // function createLink(search) {
+            //Take the Value of City name, connected to a link containing the BASE_URL used in fetch.
+
+          
+           
+           
+
+            const ul = document.querySelector("ul")
+            const li = document.createElement("li")
+            // const li = document.createElement("li")
+            const a = document.createElement("a");
+            function addToList(url, area, current) {
+                li;
+                a;
+                a.setAttribute("href", `${url}`);
+                a.innerHTML = url;
+                a.innerText = `${area} - ${current}°F`
+                // listArr.push(a.innerText);
+                li.appendChild(a);
+                ul.append(li);
+                
+            }
+            addToList(BASE_URL, nearestArea, currentlyF);
+    // console.log(listArr)
+    
+            // const a = document.createElement("a");
+            // a.setAttribute("href", `${BASE_URL}`);
+            // a.innerHTML = BASE_URL;
+            // a.innerText = `${nearestArea} - ${currentlyF}`
+            // const ulLi = document.querySelectorAll("li")
+//  const listArr = [...new Set(ul)]
+//  console.log(listArr)
+
+            // li.appendChild(a);
+            // ul.append(li);
+           
+            console.log(li)
+        
+            // const unordered = "<li>" + 
+            // ulLi.forEach((listItem)=> {
+                
+                
+            //     console.log(li.innerHTML)
+            //         console.log(listItem.innerHTML)
+                
+            // })
+
+            // const ulLiArr = [...new Set(ulLi)]
+           //[bronx, big city, melbourne]
+  
+        //   console.log(ulLiArr)
+          
             
-            
-        }
-        //edit this later
-        console.log(json, "console log json");
+
+
+            // for (let index = 0; index < ulLi.length; index++) {
+    
+            //     console.log(ulLi[index])
+                
+            // }
+        
+        //   a.document.write(nearestArea.link(BASE_URL));
+        // a.href = `${BASE_URL}`;
+        // a.innerText = nearestArea;
+        // li.appendChild(a)
+        // a.append(li)
+       
+
+        // }
+        //  const p = document.createElement("p");
+
+        //   console.log(ul)
+        //   console.log(a)
+        //   console.log(li)
+        //   console.log(ul)
+
+          
+        //   li.append(p);
+          
+
+
+
+          a.addEventListener("click", (event) => {
+            event.preventDefault();
+            const allLi = document.querySelectorAll("li");
+            const newA = document.createElement("a");
+        newA.setAttribute("href", `${BASE_URL}`);
+        newA.innerHTML = BASE_URL;
+        // console.log(a)
+        fetch(a)
+        .then((response) => response.json())
+        .then((json) => {
+        
+        inputHeading.innerHTML = prettyInput;
+        areaP.innerHTML = `<strong>Area: </strong>${nearestArea}`;
+        regionP.innerHTML = `<strong>Region: </strong>${region}`;
+        countryP.innerHTML = `<strong>Country:</strong> ${country}`;
+        currentlyFP.innerHTML = `<strong>Currently: </strong>${currentlyF}`;
+        todayAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${todayAvgTempJson}°F`;
+        todayMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${todayMaxTempJson}°F`;
+    todayMinTemp.innerHTML = `<strong>Min Temperature: </strong> ${todayMinTempJson}°F`;
+   tmmAvgTemp.innerHTML = `<strong>Average Temperature: </strong>${tmmAvgTempJson}°F`;
+    tmmMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${tmmMaxTempJson}°F`;
+  tmmMinTemp.innerHTML = `<strong>Min Temperature: </strong>${tmmMinTempJson}°F`;
+  dayAfterAvgTemp.innerHTML = innerHTML = `<strong>Average Temperature: </strong>${dayAfterAvgTempJson}°F`;
+   dayAfterMaxTemp.innerHTML = `<strong>Max Temperature: </strong>${dayAfterMaxTempJson}°F`;
+   dayAfterMinTemp.innerHTML = `<strong>Min Temperature: </strong>${dayAfterMinTempJson}°F`;
+
+
+        
+      
+
+
+            // console.log(clickJson)
+        
+        })
+
+        // for (let li of allLi) {
+
+        //     if (li.innerHTML === newA.innerHTML) {
+        //         event.target.parentNode.remove();
+        // }
+        // }
+
+        
+    })
+    
     })
     .catch((error) => {
         console.log(error, "error here!")
